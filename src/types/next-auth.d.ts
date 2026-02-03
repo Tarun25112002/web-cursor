5fc7aa4326fcaaf5ea7981abefd83d478555aadf
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2026-02-03 11:20:26 +0530

    NextAuth Configuration

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,24 @@
+import { UserRole } from "@prisma/client";
+import NextAuth, { type DefaultSession } from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+  isTwoFactorEnabled: boolean;
+  isOAuth: boolean;
+  isActive: boolean;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: UserRole;
+    isTwoFactorEnabled?: boolean;
+    isOAuth?: boolean;
+    isActive?: boolean;
+  }
+}
