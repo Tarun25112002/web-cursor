1f9add2636e296e162fa2f7db9e1a814f5f2b745
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2026-02-03 14:50:59 +0530

    lib/auth.ts

---
+++ src/lib/auth.config.ts
@@ -0,0 +1,58 @@
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
+import bcrypt from "bcryptjs";
+import { LoginSchema } from "./validation";
+import { db } from "./db";
+
+export default {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (!validatedFields.success) {
+          return null;
+        }
+
+        const { email, password } = validatedFields.data;
+
+        const user = await db.user.findUnique({
+          where: { email },
+        });
+
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordsMatch) {
+          return null;
+        }
+
+        if (!user.isActive) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
